c4f40c78b113a0b576c8e6b5290b5c6120504e70
Abel S. Salvatierra <assalvatierra@gmail.com>
2022-04-22 21:44:06 -0700

    Added-category-fields-in-InvItem
    added category fields in InvItem.
    added category forms

---
+++ InvWeb/Pages/Masterfiles/ItemMaster/Categories/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using InvWeb.Data;
+using WebDBSchema.Models;
+
+namespace InvWeb.Pages.Masterfiles.ItemMaster.Categories
+{
+    public class IndexModel : PageModel
+    {
+        private readonly InvWeb.Data.ApplicationDbContext _context;
+
+        public IndexModel(InvWeb.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<InvCategory> InvCategory { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            InvCategory = await _context.InvCategory.ToListAsync();
+        }
+    }
+}
