41139318175f6e8bb450e3a29a791187f07f653a
Abel S. Salvatierra <assalvatierra@gmail.com>
2022-04-23 10:38:00 -0700

    Add test folder for test pages
    Add xTestPages for test pages

---
+++ InvWeb/Pages/xTestPages/Index.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using InvWeb.Data;
+using WebDBSchema.Models;
+
+namespace InvWeb.Pages.xTestPages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly InvWeb.Data.ApplicationDbContext _context;
+
+        public IndexModel(InvWeb.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<InvItem> InvItem { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            try
+            {
+                InvItem = await _context.InvItems
+                    .Include(i => i.InvCategory).ToListAsync();
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.Message);
+            }
+
+
+
+        }
+    }
+}
