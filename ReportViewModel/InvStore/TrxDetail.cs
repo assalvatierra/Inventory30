3a5692283274891649dd190c33edd8e71ceb4bb9
Abel Salvatierra <assalvatierra@gmail.com>
2022-05-18 20:25:16 -0700

    added sample report views

---
+++ ReportViewModel/InvStore/TrxDetail.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReportViewModel.InvStore
+{
+    public class TrxDetail
+    {
+        public int Id { get; set; }
+        public int TrxHdrId { get; set; }
+        public string Description { get; set; }
+        public string Remarks { get; set; }
+
+    }
+}
