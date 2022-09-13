0f2b82c22d4dc2b71bbd3397b6156f166e23962f
jahdiel <jahdielsvillosa@gmail.com>
2022-09-13 16:34:09 +0800

    Add / Update Initialize SQL script
    Add Vsteel Initalization scripts
    Add Common Intialization scripts
    Rename Realsys and vpro scripts

--- WebDBSchema/WebDBSchema/Models/SQL Scripts/Common/202205.v1.001.Initial.sql
+++ WebDBSchema/WebDBSchema/Models/SQL Scripts/Common/202205.v1.001.Initial.sql
@@ -3,7 +3,7 @@
 IF NOT EXISTS (SELECT 1 FROM InvStores)
 BEGIN
 	insert into InvStores("StoreName") values 
-	('Cebu Branch 1'),('Davao Main'),('Gensan Branch 1');
+	('Davao Main');
 END
 
 --Unit of Measure--
---
+++ WebDBSchema/WebDBSchema/Models/SQL Scripts/VSteel/202205.v1.001.Initial.sql
@@ -0,0 +1,7 @@
+﻿-- Stores --
+
+IF NOT EXISTS (SELECT 1 FROM InvStores)
+BEGIN
+	insert into InvStores("StoreName") values 
+	('Davao Main');
+END
