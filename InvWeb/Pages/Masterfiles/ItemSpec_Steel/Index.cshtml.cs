6bf600d4f510edee5c1946ba0515795c22eaaa0b
jahdiel <jahdielsvillosa@gmail.com>
2022-09-26 19:26:45 +0800

    Added Mastefile pages for ItemSpec_Steel

---
+++ InvWeb/Pages/Masterfiles/ItemSpec_Steel/Index.cshtml.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebDBSchema.Models;
+
+namespace InvWeb.Pages.Masterfiles.ItemSpec_Steel
+{
+    public class IndexModel : PageModel
+    {
+
+        private readonly InvWeb.Data.ApplicationDbContext _context;
+
+        public IndexModel(InvWeb.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IList<InvItemSpec_Steel> InvItemSpec_Steel { get; set; }
+
+        public async Task OnGetAsync()
+        {
+            InvItemSpec_Steel = await _context.InvItemSpec_Steel
+                .Include(i => i.InvItem)
+                .ToListAsync();
+        }
+    }
+}
