e57d96711a0663124ac3a07a8bbd2393366199fa
jahdiel <jahdielsvillosa@gmail.com>
2022-10-06 18:46:42 +0800

    Create Api controller for Categories

---
+++ InvWeb/Api/CategoriesController.cs
@@ -0,0 +1,57 @@
+﻿using InvWeb.Data.Services;
+using InvWeb.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using WebDBSchema.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+
+namespace InvWeb.Api
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+
+        private readonly ApplicationDbContext _context;
+        private readonly ItemServices itemServices;
+
+        public CategoriesController(ApplicationDbContext context)
+        {
+            _context = context;
+            itemServices = new ItemServices(context);
+        }
+
+
+        // GET: api/Categories/GetCategories
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<InvItem>>> GetCategories()
+        {
+            return await _context.InvItems.ToListAsync();
+        }
+
+        // GET: api/Categories/GetCategoryDefinedSpecsCount
+        [Route("api/Categories/GetCategoryDefinedSpecsCount/{id}")]
+        [HttpGet("{id}")]
+        public async Task<ActionResult<int>> GetCategoryDefinedSpecsCount(int id)
+        {
+            if (id == 0)
+            {
+                return 0;
+            }
+            var itemCategory = await _context.InvCategories
+                                    .Include(i=>i.InvCategorySpecDefs)
+                                    .Where(i=>i.Id == id)
+                                    .ToListAsync();
+            if (itemCategory != null)
+            {
+                return itemCategory.FirstOrDefault().InvCategorySpecDefs.Count();
+            }
+
+            return 0;
+        }
+
+    }
+}
