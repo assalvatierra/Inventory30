356e015c434a54d274f9875abfad9b31d3a7ef64
jahdiel <jahdielsvillosa@gmail.com>
2022-10-11 17:25:42 +0800

    Added CustomSpec on ItemCategories

---
+++ InvWeb/Pages/Masterfiles/ItemCategories/CustomSpec/Add.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using InvWeb.Data;
+using WebDBSchema.Models;
+
+namespace InvWeb.Pages.Masterfiles.ItemCategories.CustomSpec
+{
+    public class AddModel : PageModel
+    {
+        private readonly InvWeb.Data.ApplicationDbContext _context;
+
+        public AddModel(InvWeb.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+        ViewData["InvCategoryId"] = new SelectList(_context.InvCategories, "Id", "Id");
+        ViewData["InvItemCustomSpecTypeId"] = new SelectList(_context.InvCustomSpecs, "Id", "Id");
+            return Page();
+        }
+
+        [BindProperty]
+        public InvCatCustomSpec InvCatCustomSpec { get; set; }
+
+        // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.InvCatCustomSpecs.Add(InvCatCustomSpec);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
