0d8e49bcdd03fe4b71b6ac997093da1e7b357981
jahdiel <jahdielsvillosa@gmail.com>
2022-10-25 17:24:31 +0800

    Remove specifications on item create/edit

--- InvWeb/Pages/Masterfiles/ItemMaster/Create.cshtml.cs
+++ InvWeb/Pages/Masterfiles/ItemMaster/Create.cshtml.cs
@@ -53,9 +53,6 @@ namespace InvWeb.Pages.Masterfiles.ItemMaster
                 categoryId = 1;
             }
 
-            //update showSpec flag based on Category
-            this.showSpec = _itemSpecServices.IsCategoryHaveSpecDefs(categoryId);
-
             ViewData["InvCategoryId"] = new SelectList(_context.Set<InvCategory>(), "Id", "Description", categoryId);
             ViewData["InvUomId"] = new SelectList(_context.Set<InvUom>(), "Id", "uom");
             return Page();
@@ -66,9 +63,6 @@ namespace InvWeb.Pages.Masterfiles.ItemMaster
 
         [BindProperty]
         public InvItemSpec_Steel InvItemSpec_Steel { get; set; }
-
-        [BindProperty]
-        public Boolean showSpec { get; set; }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
         public async Task<IActionResult> OnPostAsync()
@@ -81,31 +75,9 @@ namespace InvWeb.Pages.Masterfiles.ItemMaster
             _context.InvItems.Add(InvItem);
             await _context.SaveChangesAsync();
 
-            await AddItemSpecToItem();
 
             return RedirectToPage("./Index");
         }
 
-        public async Task<int> AddItemSpecToItem()
-        {
-            if (!ModelState.IsValid)
-            {
-                return 0;
-            }
-
-            if (InvItem.Id == 0 || InvItem == null)
-            {
-                return 0;
-            }
-
-            if (String.IsNullOrEmpty(InvItemSpec_Steel.SpecFor))
-            {
-                return 0;
-            }
-
-            InvItemSpec_Steel.InvItemId = InvItem.Id;
-
-            return await _itemSpecServices.AddItemSpecification(InvItemSpec_Steel);
-        }
     }
 }
