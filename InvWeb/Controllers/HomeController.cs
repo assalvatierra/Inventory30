62c1927df87bd65f401a4660229f372de3e5863e
Abel S. Salvatierra <assalvatierra@gmail.com>
2023-02-18 00:18:57 +0000

    added-sample-report

---
+++ InvWeb/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using DevExpress.AspNetCore.Reporting.WebDocumentViewer;
+using DevExpress.XtraReports.Web.WebDocumentViewer;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using InvWeb.Data;
+using InvWeb.Services.PredefinedReports;
+
+namespace InvWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public async Task<IActionResult> Viewer(
+            [FromServices] IWebDocumentViewerClientSideModelGenerator clientSideModelGenerator,
+            [FromQuery] string reportName)
+        {
+
+            //var reportToOpen = string.IsNullOrEmpty(reportName) ? "TestReport" : reportName;
+            var reportToOpen = "TestReport";
+
+            var model = new Models.ViewerModel
+            {
+                ViewerModelToBind = await clientSideModelGenerator.GetModelAsync(reportToOpen, WebDocumentViewerController.DefaultUri)
+            };
+            return View(model);
+        }
+
+    }
+}
