3de01396ee1df1f4a2d9a82970dd7fdb1a6f8cad
Abel Salvatierra <assalvatierra@gmail.com>
2023-02-28 22:02:00 -0800

    ejobs30 initial project

---
+++ UI/Projects/eJob30/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace eJob30.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
