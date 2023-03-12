1201393d8ef66d20afe9b2ddff0dc00e21142593
Abel Salvatierra <assalvatierra@gmail.com>
2023-03-12 13:50:40 -0700

    add report designer
    add report designer

--- InvWeb/Controllers/HomeController.cs
+++ InvWeb/Controllers/HomeController.cs
@@ -4,6 +4,12 @@ using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 using InvWeb.Data;
 using InvWeb.Services.PredefinedReports;
+using DevExpress.AspNetCore.Reporting.QueryBuilder;
+using DevExpress.AspNetCore.Reporting.ReportDesigner;
+using DevExpress.DataAccess.Sql;
+using DevExpress.XtraReports.UI;
+using DevExpress.XtraReports.Web.ReportDesigner;
+using System.Collections.Generic;
 
 namespace InvWeb.Controllers
 {
@@ -13,6 +19,40 @@ namespace InvWeb.Controllers
         {
             return View();
         }
+
+        public async Task<IActionResult> Designer(
+            [FromServices] IReportDesignerClientSideModelGenerator clientSideModelGenerator,
+            [FromQuery] string reportName)
+        {
+            InvWeb.Reporting.Models.ReportDesignerCustomModel model = new InvWeb.Reporting.Models.ReportDesignerCustomModel();
+            model.ReportDesignerModel = await CreateDefaultReportDesignerModel(clientSideModelGenerator, reportName, null);
+            return View(model);
+        }
+
+        public static Dictionary<string, object> GetAvailableDataSources()
+        {
+            var dataSources = new Dictionary<string, object>();
+            // Create a SQL data source with the specified connection string.
+            SqlDataSource ds = new SqlDataSource("NWindConnectionString");
+            // Create a SQL query to access the Products data table.
+            SelectQuery query = SelectQueryFluentBuilder.AddTable("Products").SelectAllColumnsFromTable().Build("Products");
+            ds.Queries.Add(query);
+            ds.RebuildResultSchema();
+            dataSources.Add("Northwind", ds);
+            return dataSources;
+        }
+
+        public static async Task<ReportDesignerModel> CreateDefaultReportDesignerModel(IReportDesignerClientSideModelGenerator clientSideModelGenerator, string reportName, XtraReport report)
+        {
+            reportName = string.IsNullOrEmpty(reportName) ? "TestReport" : reportName;
+            var dataSources = GetAvailableDataSources();
+            if (report != null)
+            {
+                return await clientSideModelGenerator.GetModelAsync(report, dataSources, ReportDesignerController.DefaultUri, WebDocumentViewerController.DefaultUri, QueryBuilderController.DefaultUri);
+            }
+            return await clientSideModelGenerator.GetModelAsync(reportName, dataSources, ReportDesignerController.DefaultUri, WebDocumentViewerController.DefaultUri, QueryBuilderController.DefaultUri);
+        }
+
 
         public async Task<IActionResult> Viewer(
             [FromServices] IWebDocumentViewerClientSideModelGenerator clientSideModelGenerator,
