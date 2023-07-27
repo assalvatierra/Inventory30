747807c31aa80a3246200784b0a36b4f76aa0879
jahdiel <jahdielsvillosa@gmail.com>
2023-07-27 13:49:44 +0800

    Remove and Update Views for ItemSpec_Steel

--- InvWeb/Api/SpecificationsController.cs
+++ InvWeb/Api/SpecificationsController.cs
@@ -135,12 +135,12 @@ namespace InvWeb.Api
                     // ADD ITEM STEEL SPEC 
                     InvItemSpec_Steel new_invItemSpec = new InvItemSpec_Steel();
                     new_invItemSpec.InvItemId = item_Spec.InvItemId;
-                    new_invItemSpec.SpecFor = item_Spec.SpecFor;
-                    new_invItemSpec.SpecInfo = item_Spec.SpecInfo;
-                    new_invItemSpec.SizeValue = item_Spec.SizeValue;
-                    new_invItemSpec.SizeDesc = item_Spec.SizeDesc;
-                    new_invItemSpec.WtValue = item_Spec.WtValue;
-                    new_invItemSpec.WtDesc = item_Spec.WtDesc;
+                    //new_invItemSpec.SpecFor = item_Spec.SpecFor;
+                    //new_invItemSpec.SpecInfo = item_Spec.SpecInfo;
+                    //new_invItemSpec.SizeValue = item_Spec.SizeValue;
+                    //new_invItemSpec.SizeDesc = item_Spec.SizeDesc;
+                    //new_invItemSpec.WtValue = item_Spec.WtValue;
+                    //new_invItemSpec.WtDesc = item_Spec.WtDesc;
                      
                     await _itemSpecServices.AddItemSpecification(new_invItemSpec);
                     return Ok();
@@ -149,12 +149,12 @@ namespace InvWeb.Api
                 InvItemSpec_Steel invItemSpec = InvSteelSpec.FirstOrDefault();
 
                 invItemSpec.InvItemId = item_Spec.InvItemId;
-                invItemSpec.SpecFor   = item_Spec.SpecFor;
-                invItemSpec.SpecInfo  = item_Spec.SpecInfo;
-                invItemSpec.SizeValue = item_Spec.SizeValue;
-                invItemSpec.SizeDesc  = item_Spec.SizeDesc;
-                invItemSpec.WtValue   = item_Spec.WtValue;
-                invItemSpec.WtDesc    = item_Spec.WtDesc;
+                //invItemSpec.SpecFor   = item_Spec.SpecFor;
+                //invItemSpec.SpecInfo  = item_Spec.SpecInfo;
+                //invItemSpec.SizeValue = item_Spec.SizeValue;
+                //invItemSpec.SizeDesc  = item_Spec.SizeDesc;
+                //invItemSpec.WtValue   = item_Spec.WtValue;
+                //invItemSpec.WtDesc    = item_Spec.WtDesc;
 
                 await _itemSpecServices.EditItemSpecification(invItemSpec);
 
