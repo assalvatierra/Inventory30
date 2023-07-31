8c9b31771e88887f81e407890f09048beb430272
jahdiel <jahdielsvillosa@gmail.com>
2023-07-31 17:51:18 +0800

    Add SteelMaterials Views

---
+++ InvWeb/Pages/Masterfiles/ItemSpecSteel/SteelMaterials/Edit.cshtml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using CoreLib.Models.Inventory;
+using CoreLib.Inventory.Models;
+
+namespace InvWeb.Pages.Masterfiles.ItemSpecSteel.SteelMaterials
+{
+    public class EditModel : PageModel
+    {
+        private readonly ApplicationDbContext _context;
+
+        public EditModel(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public SteelMaterial SteelMaterial { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.SteelMaterials == null)
+            {
+                return NotFound();
+            }
+
+            var steelmaterial =  await _context.SteelMaterials.FirstOrDefaultAsync(m => m.Id == id);
+            if (steelmaterial == null)
+            {
+                return NotFound();
+            }
+            SteelMaterial = steelmaterial;
+            return Page();
+        }
+
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see https://aka.ms/RazorPagesCRUD.
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(SteelMaterial).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!SteelMaterialExists(SteelMaterial.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool SteelMaterialExists(int id)
+        {
+          return (_context.SteelMaterials?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
