6d3b7616c58d47327770588b9df16ddb86fdd22b
jahdiel <jahdielsvillosa@gmail.com>
2023-08-05 16:29:14 +0800

    Add SysLabel Table on InvDb

---
+++ CoreLib/Models/Inventory/SysLabel.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace CoreLib.Models.Inventory
+{
+    public partial class SysLabel
+    {
+        public int Id { get; set; }
+        public string? Code { get; set; }
+        public string? DisplayText { get; set; }
+        public string? DisplayLang { get; set; }
+    }
+}
