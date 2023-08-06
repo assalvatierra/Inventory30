058b2d6b26be39fe6f1c4013ded0b248f887defa
Abel Salvatierra <assalvatierra@gmail.com>
2023-08-05 22:09:13 -0700

    2028-08-05-AppSettings - Warehouse

---
+++ InvWeb/Shared/SystemSetting.cs
@@ -0,0 +1,19 @@
+﻿namespace InvWeb.Shared
+{
+    public class SystemSetting
+    {
+        public string GetValue(string sCode)
+        {
+            string sLabel = sCode;
+
+            if (sCode == "Store") sLabel = "Warehouse";
+            if (sCode == "AppName") sLabel = "Inventory";
+            if (sCode == "AppVersion") sLabel = "Beta 1.1";
+            if (sCode == "Tenant") sLabel = "RealSteel";
+
+            return sLabel;
+
+        }
+
+    }
+}
