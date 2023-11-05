8fa817ef82d07d7db0c0e966a65bc1ac2e058d5b
Abel Salvatierra <assalvatierra@gmail.com>
2023-11-05 10:03:10 -0800

    SampleMvcController

---
+++ InvWeb/Controllers/SampleMvcController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace InvWeb.Controllers
+{
+    public class SampleMvcController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
