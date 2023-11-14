b65c208fee47bdee248469c8bf51c44e04e2a495
jahdiel <jahdielsvillosa@gmail.com>
2023-11-14 17:13:40 +0800

    Dialog Item cs Class file

---
+++ CoreLib/DTO/Common/Dialog/DialogItems.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreLib.DTO.Common.Dialog
+{
+    public class DialogItems
+    {
+            public int Id { get; set; }
+            public string? Name { get; set; }
+            public string? Description { get; set; }
+        
+    }
+}
