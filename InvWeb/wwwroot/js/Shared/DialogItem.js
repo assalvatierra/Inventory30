788a0f1289a4e8e6c3e7b6984d01f1debc25156e
jahdiel <jahdielsvillosa@gmail.com>
2023-11-15 14:28:43 +0800

    Item Search - Update script file and view margin
    +delete Component1 View/cshtml (unecessary)
    +separate script for dialog modal into dialogitem.js
    +adjust dropdown input width to match the rest of the inputs in the form

---
+++ InvWeb/wwwroot/js/Shared/DialogItem.js
@@ -0,0 +1,60 @@
+﻿/**
+ *  DialogItem.js
+ *  Used for Component page /Pages/Shared/Default.cshtml
+ *  Modal Show/Hide, and Search
+ * 
+ *  Nov 15 2023
+ */
+
+
+
+$("#itemDropdown").focus(function () {
+    $("#itemDropdown > option").css("display", "none");
+
+    ShowItemModal();
+});
+
+function ShowItemModal() {
+    $("#itemSearchModal").modal("show");
+
+}
+
+
+function HideItemModal() {
+    $("#itemSearchModal").modal("hide");
+
+}
+
+function SelectItemFromDialog(selector, id) {
+    //update selected in dropdown
+    $("#" + selector).val(id).change();
+    HideItemModal();
+
+}
+
+
+function SearchCompany() {
+    var input, filter, ul, li, a, i;
+    input = $("#SearchBar").val();
+    filter = input;
+    ul = document.getElementById("SearchList");
+    tr = ul.getElementsByTagName("tr");
+
+
+
+    $("#SearchList tr").each(function () {
+        var itemText = $(this).find("td").eq(1).text();
+
+        if (itemText != undefined) {
+            if (itemText.toLowerCase().indexOf(filter) >= 0) {
+               
+                $(this).show();
+            } else {
+                $(this).hide();
+            }
+
+        }
+
+    });
+
+}
